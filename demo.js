83cd9cb3258eff2e8cf481d9888f40a24b295bb7
miffy24 <chenzhutao125@163.com>
2017-12-18 13:56:53 +0800

    修改换行

--- demo.js
+++ demo.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
-var fs = require('fs')
-
+ var fs = require('fs')
  var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
-
  process.chdir("/Users/chen/Desktop") // cd ~/Desktop
  fs.mkdirSync("./" + dirName) // mkdir $1
  process.chdir("./" + dirName) // cd $1
  fs.mkdirSync('css') // mkdir css
  fs.mkdirSync('js') // mkdir js
- fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
+ fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
  fs.writeFileSync("css/style.css", "h1{color:red;}")
- fs.writeFileSync("./js/main.js", "var string = 'Hello World'alert(string)")
+ fs.writeFileSync("./js/main.js", 'var string = \"Hello World"\nalert(string)')
 
  process.exit(0)
